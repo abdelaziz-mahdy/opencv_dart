c9bdbe91beded43336e58d1175063ec57df1ab2b
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-23 16:40:50 +0300

    initial setup for ml

---
+++ src/ml/ml.cpp
@@ -0,0 +1,189 @@
+/* This file is originally from gocv project Licensed: Apache 2.0 license. Copyright (c) 2017-2021 The Hybrid Group. Modified by Rainyl. Licensed: Apache 2.0 license. Copyright (c) 2024 Rainyl. */
+
+#include "ml.h"
+#include <memory>
+#include <vector>
+
+// KNearest
+CvStatus KNearest_Create(KNearest *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::KNearest>(cv::ml::KNearest::create())};
+    END_WRAP
+}
+
+CvStatus KNearest_Close(KNearest *knn) {
+    CVD_FREE(knn)
+}
+
+CvStatus KNearest_Train(KNearest knn, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    knn.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus KNearest_FindNearest(KNearest knn, Mat samples, int k, Mat results, Mat neighborResponses, Mat dists) {
+    BEGIN_WRAP
+    knn.ptr->findNearest(*samples.ptr, k, *results.ptr, *neighborResponses.ptr, *dists.ptr);
+    END_WRAP
+}
+
+// SVM
+CvStatus SVM_Create(SVM *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::SVM>(cv::ml::SVM::create())};
+    END_WRAP
+}
+
+CvStatus SVM_Close(SVM *svm) {
+    CVD_FREE(svm)
+}
+
+CvStatus SVM_Train(SVM svm, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    svm.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus SVM_Predict(SVM svm, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    svm.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// DecisionTree
+CvStatus DTrees_Create(DTrees *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::DTrees>(cv::ml::DTrees::create())};
+    END_WRAP
+}
+
+CvStatus DTrees_Close(DTrees *dtree) {
+    CVD_FREE(dtree)
+}
+
+CvStatus DTrees_Train(DTrees dtree, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    dtree.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus DTrees_Predict(DTrees dtree, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    dtree.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// RandomForest
+CvStatus RTrees_Create(RTrees *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::RTrees>(cv::ml::RTrees::create())};
+    END_WRAP
+}
+
+CvStatus RTrees_Close(RTrees *rtree) {
+    CVD_FREE(rtree)
+}
+
+CvStatus RTrees_Train(RTrees rtree, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    rtree.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus RTrees_Predict(RTrees rtree, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    rtree.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// Boost
+CvStatus Boost_Create(Boost *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::Boost>(cv::ml::Boost::create())};
+    END_WRAP
+}
+
+CvStatus Boost_Close(Boost *boost) {
+    CVD_FREE(boost)
+}
+
+CvStatus Boost_Train(Boost boost, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    boost.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus Boost_Predict(Boost boost, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    boost.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// NeuralNetwork
+CvStatus ANN_MLP_Create(ANN_MLP *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::ANN_MLP>(cv::ml::ANN_MLP::create())};
+    END_WRAP
+}
+
+CvStatus ANN_MLP_Close(ANN_MLP *ann) {
+    CVD_FREE(ann)
+}
+
+CvStatus ANN_MLP_Train(ANN_MLP ann, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    ann.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus ANN_MLP_Predict(ANN_MLP ann, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    ann.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// LogisticRegression
+CvStatus LogisticRegression_Create(LogisticRegression *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::LogisticRegression>(cv::ml::LogisticRegression::create())};
+    END_WRAP
+}
+
+CvStatus LogisticRegression_Close(LogisticRegression *logreg) {
+    CVD_FREE(logreg)
+}
+
+CvStatus LogisticRegression_Train(LogisticRegression logreg, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    logreg.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus LogisticRegression_Predict(LogisticRegression logreg, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    logreg.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
+
+// NormalBayesClassifier
+CvStatus NormalBayesClassifier_Create(NormalBayesClassifier *rval) {
+    BEGIN_WRAP
+    *rval = {new cv::Ptr<cv::ml::NormalBayesClassifier>(cv::ml::NormalBayesClassifier::create())};
+    END_WRAP
+}
+
+CvStatus NormalBayesClassifier_Close(NormalBayesClassifier *nbayes) {
+    CVD_FREE(nbayes)
+}
+
+CvStatus NormalBayesClassifier_Train(NormalBayesClassifier nbayes, Mat samples, int layout, Mat responses) {
+    BEGIN_WRAP
+    nbayes.ptr->train(*samples.ptr, layout, *responses.ptr);
+    END_WRAP
+}
+
+CvStatus NormalBayesClassifier_Predict(NormalBayesClassifier nbayes, Mat samples, Mat results, int flags) {
+    BEGIN_WRAP
+    nbayes.ptr->predict(*samples.ptr, *results.ptr, flags);
+    END_WRAP
+}
---
+++ src/ml/ml.h
@@ -0,0 +1,84 @@
+#ifndef _OPENCV3_ML_H_
+#define _OPENCV3_ML_H_
+
+#include "core/core.h"
+#include <stdbool.h>
+
+#ifdef __cplusplus
+#include <opencv2/ml.hpp>
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+CVD_TYPEDEF(cv::Ptr<cv::ml::NormalBayesClassifier>, NormalBayesClassifier)
+CVD_TYPEDEF(cv::Ptr<cv::ml::KNearest>, KNearest)
+CVD_TYPEDEF(cv::Ptr<cv::ml::SVM>, SVM)
+CVD_TYPEDEF(cv::Ptr<cv::ml::EM>, EM)
+CVD_TYPEDEF(cv::Ptr<cv::ml::DTrees>, DTrees)
+CVD_TYPEDEF(cv::Ptr<cv::ml::RTrees>, RTrees)
+CVD_TYPEDEF(cv::Ptr<cv::ml::Boost>, Boost)
+CVD_TYPEDEF(cv::Ptr<cv::ml::ANN_MLP>, ANN_MLP)
+CVD_TYPEDEF(cv::Ptr<cv::ml::LogisticRegression>, LogisticRegression)
+CVD_TYPEDEF(cv::Ptr<cv::ml::SVMSGD>, SVMSGD)
+#else
+CVD_TYPEDEF(void, NormalBayesClassifier)
+CVD_TYPEDEF(void, KNearest)
+CVD_TYPEDEF(void, SVM)
+CVD_TYPEDEF(void, EM)
+CVD_TYPEDEF(void, DTrees)
+CVD_TYPEDEF(void, RTrees)
+CVD_TYPEDEF(void, Boost)
+CVD_TYPEDEF(void, ANN_MLP)
+CVD_TYPEDEF(void, LogisticRegression)
+CVD_TYPEDEF(void, SVMSGD)
+#endif
+
+CVD_TYPEDEF_PTR(NormalBayesClassifier)
+CVD_TYPEDEF_PTR(KNearest)
+CVD_TYPEDEF_PTR(SVM)
+CVD_TYPEDEF_PTR(EM)
+CVD_TYPEDEF_PTR(DTrees)
+CVD_TYPEDEF_PTR(RTrees)
+CVD_TYPEDEF_PTR(Boost)
+CVD_TYPEDEF_PTR(ANN_MLP)
+CVD_TYPEDEF_PTR(LogisticRegression)
+CVD_TYPEDEF_PTR(SVMSGD)
+
+CvStatus NormalBayesClassifier_Create(NormalBayesClassifier* rval);
+CvStatus KNearest_Create(KNearest* rval);
+CvStatus SVM_Create(SVM* rval);
+CvStatus EM_Create(EM* rval);
+CvStatus DTrees_Create(DTrees* rval);
+CvStatus RTrees_Create(RTrees* rval);
+CvStatus Boost_Create(Boost* rval);
+CvStatus ANN_MLP_Create(ANN_MLP* rval);
+CvStatus LogisticRegression_Create(LogisticRegression* rval);
+CvStatus SVMSGD_Create(SVMSGD* rval);
+
+void NormalBayesClassifier_Close(NormalBayesClassifier* self);
+void KNearest_Close(KNearest* self);
+void SVM_Close(SVM* self);
+void EM_Close(EM* self);
+void DTrees_Close(DTrees* self);
+void RTrees_Close(RTrees* self);
+void Boost_Close(Boost* self);
+void ANN_MLP_Close(ANN_MLP* self);
+void LogisticRegression_Close(LogisticRegression* self);
+void SVMSGD_Close(SVMSGD* self);
+
+CvStatus NormalBayesClassifier_Predict(NormalBayesClassifier self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus KNearest_Predict(KNearest self, Mat samples, CVD_OUT Mat* results, int k, float* rval);
+CvStatus SVM_Predict(SVM self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus EM_Predict(EM self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus DTrees_Predict(DTrees self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus RTrees_Predict(RTrees self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus Boost_Predict(Boost self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus ANN_MLP_Predict(ANN_MLP self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus LogisticRegression_Predict(LogisticRegression self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+CvStatus SVMSGD_Predict(SVMSGD self, Mat samples, CVD_OUT Mat* results, int flags, float* rval);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_OPENCV3_ML_H_
